37af60adbcb8f32ca1edd86519b52fb33c1f9eb0
김용훈 <bluepoet1112@gmail.com>
2014-11-29 22:56:17 +0900

    SQL CREATE TABLE DESCRIPTION INITIAL COMMIT

---
+++ sql/member_match_list.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `member_match_list` (
+  `first_id` INT(11) DEFAULT NULL,
+  `after_id` INT(11) DEFAULT NULL,
+  `create_date` DATETIME DEFAULT NULL
+) ENGINE=INNODB DEFAULT CHARSET=utf8
